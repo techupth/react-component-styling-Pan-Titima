8d90d15414e617a02cf1f9f34dae96ade00c306c
Titima Kasemsritanawat <titima.kp@gmail.com>
2024-08-12 19:19:37 +0700

    Answer Ex - Alert box

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -7,87 +7,149 @@ import CircleIcon from "../../alert-circle.svg";
 import TriangleIcon from "../../alert-triangle.svg";
 import FrownIcon from "../../frown.svg";
 
-
-
 export function SuccessAlert() {
-
-    return ( 
-       
-        <div css={css`
-            width: 650px;
-            height: 76px;
-            gap: 0px;
-            border-radius: 10px 0px 0px 0px;
-            opacity: 0px;
-            background-color: #CEF7CD;
-            margin: 20px;
-
-         `}>
-            <img src={CheckIcon} css={css `padding-top: 30px;`}></img>
-               This is success alert box
-            
-        </div>
-    );    
+  return (
+    <div
+      css={css`
+        margin: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        padding-left: 20px;
+        width: 650px;
+        height: 76px;
+        font-family: "Kanit", sans-serif;
+        font-weight: 700;
+        font-size: 20px;
+        color: #444444;
+        border-radius: 10px;
+        background-color: #cef7cd;
+      `}
+    >
+      <div
+        css={css`
+          margin-right: 20px;
+        `}
+      >
+        <img
+          src={CheckIcon}
+          css={css`
+            margin-right: 20px;
+          `}
+        ></img>
+      </div>
+      This is success alert box
+    </div>
+  );
 }
 
 export function InfoAlert() {
+  return (
+    <div
+      css={css`
+        margin: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        padding-left: 20px;
+        width: 650px;
+        height: 76px;
+        font-family: "Kanit", sans-serif;
+        font-weight: 700;
+        font-size: 20px;
+        color: #444444;
+        border-radius: 10px;
+        background-color: #F9EBC8;
 
-    return ( 
-        <div css={css`
-            width: 650px;
-            height: 76px;
-            gap: 0px;
-            border-radius: 10px 0px 0px 0px;
-            opacity: 0px;
-            background-color: #F9EBC8;
-            margin: 20px;
-        
-        `}>
-            <img src={CircleIcon} css={`position relative; 
-                padding-top: 20px;
-                padding-right: 600px;` }></img>
-        </div>
-    );    
+      `}
+    >
+      <div
+        css={css`
+          margin-right: 20px;
+        `}
+      >
+        <img
+          src={CircleIcon}
+          css={css`
+            margin-right: 20px;
+          `}
+        ></img>
+      </div>
+      This is info alert box
+    </div>
+  );
 }
 
 export function WarningAlert() {
+  return (
+    <div
+      css={css`
+        margin: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        padding-left: 20px;
+        width: 650px;
+        height: 76px;
+        font-family: "Kanit", sans-serif;
+        font-weight: 700;
+        font-size: 20px;
+        color: #444444;
+        border-radius: 10px;
+        background-color: #F9D9C8;
 
-    return ( 
-        <div
+      `}
+    >
+      <div
         css={css`
-            width: 650px;
-            height: 76px;
-            gap: 0px;
-            border-radius: 10px 0px 0px 0px;
-            opacity: 0px;
-            background-color: #F9D9C8;
-            margin: 20px;
-        
-        `}>
-            <img src={TriangleIcon} css={`position relative; 
-                padding-top: 20px;
-                padding-right: 600px;` }></img>
-        </div>
-    );    
+          margin-right: 20px;
+        `}
+      >
+        <img
+          src={TriangleIcon}
+          css={css`
+            margin-right: 20px;
+          `}
+        ></img>
+      </div>
+      This is warning alert box
+    </div>
+  );
 }
 
 export function ErrorAlert() {
+  return (
+    <div
+      css={css`
+        margin: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        padding-left: 20px;
+        width: 650px;
+        height: 76px;
+        font-family: "Kanit", sans-serif;
+        font-weight: 700;
+        font-size: 20px;
+        color: #444444;
+        border-radius: 10px;
+        background-color: #F9C8C8;
 
-    return ( 
-        <div
+      `}
+    >
+      <div
         css={css`
-          width: 650px;
-          height: 76px;
-          gap: 0px;
-          border-radius: 10px 0px 0px 0px;
-          opacity: 0px;
-          background-color: #F9C8C8;
-          margin: 20px;
-        `}>
-            <img src={FrownIcon} css={`position relative; 
-                padding-top: 20px;
-                padding-right: 600px;` }></img>
-        </div>
-    );    
+          margin-right: 20px;
+        `}
+      >
+        <img
+          src={FrownIcon}
+          css={css`
+            margin-right: 20px;
+          `}
+        ></img>
+      </div>
+      This is error alert box
+    </div>
+  );
 }
- 
